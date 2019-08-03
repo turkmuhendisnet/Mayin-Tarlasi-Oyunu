f454dba48754f8e930336cc67986e3ccdc753360
Turkmuhendisnet <48014368+turkmuhendisnet@users.noreply.github.com>
2019-08-03 13:59:04 +0300

    Mayın Tarlası Oyunu
    Mayın Tarlası

---
+++ MayınTarlası/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MayınTarlası
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.FlpPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.tmrZaman = new System.Windows.Forms.Timer(this.components);
+            this.btnYeniden = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FlpPanel
+            // 
+            this.FlpPanel.Location = new System.Drawing.Point(12, 12);
+            this.FlpPanel.Name = "FlpPanel";
+            this.FlpPanel.Size = new System.Drawing.Size(555, 407);
+            this.FlpPanel.TabIndex = 0;
+            // 
+            // tmrZaman
+            // 
+            this.tmrZaman.Tick += new System.EventHandler(this.tmrZaman_Tick);
+            // 
+            // btnYeniden
+            // 
+            this.btnYeniden.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnYeniden.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnYeniden.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYeniden.Location = new System.Drawing.Point(197, 439);
+            this.btnYeniden.Name = "btnYeniden";
+            this.btnYeniden.Size = new System.Drawing.Size(162, 34);
+            this.btnYeniden.TabIndex = 1;
+            this.btnYeniden.Text = "Yeniden Başla";
+            this.btnYeniden.UseVisualStyleBackColor = false;
+            this.btnYeniden.Click += new System.EventHandler(this.btnYeniden_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 485);
+            this.Controls.Add(this.btnYeniden);
+            this.Controls.Add(this.FlpPanel);
+            this.Name = "Form1";
+            this.Text = "Mayın Tarlası";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel FlpPanel;
+        private System.Windows.Forms.Timer tmrZaman;
+        private System.Windows.Forms.Button btnYeniden;
+    }
+}
+
---
+++ MayınTarlası/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayınTarlası
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int[] Mayınlar = new int[20];
+        int Mavi;
+        int Yesil;
+        int Kirmizi;
+        int ButonNumarası;
+        int Puan=0;
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            for (int i = 1; i < 166; i++)
+            {
+                Kirmizi = rnd.Next(0, 255);
+                Yesil = rnd.Next(0, 255);
+                Mavi = rnd.Next(0, 255);
+                ButonNumarası = rnd.Next(0, 166);
+                Button buton = new Button();
+                buton.BackColor = Color.FromArgb(Kirmizi, Yesil, Mavi);
+                buton.BackColor = Color.FromArgb(Kirmizi, Yesil, Mavi);
+                buton.Width = 30;
+                buton.Height = 30;
+                buton.Text = ButonNumarası.ToString();
+                buton.Click += new EventHandler(buton_Click);
+                FlpPanel.Controls.Add(buton);
+            }
+            tmrZaman.Start();
+            int sayi = 0;
+            while (sayi < Mayınlar.Length)
+            {
+                int rastgeleMayın = rnd.Next(1, 166);
+                // Mayınlara Rastgele sayı değeri atandı
+                if (Mayınlar.Contains(rastgeleMayın) == false)
+                {
+                    Mayınlar[sayi] = rastgeleMayın;
+                    sayi++;
+                }
+            }
+
+        }
+
+        private void tmrZaman_Tick(object sender, EventArgs e)
+        {
+            foreach (Control item in FlpPanel.Controls)
+            {
+                if (item is Button)
+                {
+                    if (item.BackColor!=Color.White)
+                    {
+                        Kirmizi = rnd.Next(1, 256);
+                        Yesil = rnd.Next(1, 256);
+                        Mavi = rnd.Next(1, 256);
+                        item.BackColor = Color.FromArgb(Kirmizi, Yesil, Mavi);
+                        item.ForeColor = Color.FromArgb(Kirmizi, Yesil, Mavi);
+                    }
+                }
+            }
+        }
+        private void buton_Click(object sender, EventArgs e)
+        {
+            int tiklanan = Convert.ToInt32((sender as Button).Text);
+
+            if (Mayınlar.Contains(tiklanan))
+            {
+                (sender as Button).BackColor = Color.Black;               
+                tmrZaman.Stop();
+                this.BackColor = Color.Red;
+                MessageBox.Show("Mayına Dokundunuz\n"+"Puanınız:"+Puan);
+                FlpPanel.Enabled = true;
+            }
+            else
+            {
+                (sender as Button).BackColor = Color.White;
+                Puan += 5;                
+            }
+        }
+       // formu yeniden yüklemek için
+        private void btnYeniden_Click(object sender, EventArgs e)
+        {
+            if (Application.OpenForms[0] == this)//Uygulamanin main form'u
+            {
+                //uygulamanin ana formunu kapatirsaniz uygulama kapanir, ana formu yeniden baslatmak uygulamayi yeniden baslatmak demektir.
+                Application.Restart();
+            }
+ 
+        }
+    }
+}
